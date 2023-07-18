9f05bca5e99b3d624d9bf9398263e506edc862f8
Jon A <jon.jumangi@gmail.com>
2023-07-18 14:05:18 -0500

    changes to display-searchjs

--- assets/js/display-search.js
+++ assets/js/display-search.js
@@ -1,6 +1,44 @@
 var resultTextEl = document.querySelector('result-text');
 var resultContentEl = document.querySelector('result-content');
 var searchFormEl = document.querySelector('search-form');
+var fetchButton = document.getElementById('search-button');
+
+
+
+
+var fetchDataFromApi = []
+
+
+
+function getApi() {
+  var requestUrl = 'https://thecocktaildb.com/api/json/v1/1/random.php';
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      //Using console.log to examine the data
+      console.log(data);
+    })
+  console.log(data)
+  //       for (var i = 0; i < data.length; i++) {
+  //         //Creating a h3 element and a p element
+  //         var userName = document.createElement('h3');
+  //         var userUrl = document.createElement('p');
+
+  //         //Setting the text of the h3 element and p element.
+  //         userName.textContent = data[i].login;
+  //         userUrl.textContent = data[i].url;
+
+  //         //Appending the dynamically generated html to the div associated with the id="users"
+  //         //Append will attach the element as the bottom most child.
+  //         usersContainer.append(userName);
+  //         usersContainer.append(userUrl);
+  //       }
+  //     });
+  // }
+  // fetchButton.addEventListener('click', getApi);
 
 
 
@@ -8,32 +46,53 @@ var searchFormEl = document.querySelector('search-form');
 
 
 
-var requestUrl = "https://thecocktaildb.com/api/json/v1/1/random.php"
+  // //calls API
+  // var requestUrl = "https://thecocktaildb.com/api/json/v1/1/random.php"
+  // $(document).ready(function () {
+  //   function fetchDataFromApi() {
+  //     $.ajax({
+  //       url: requestUrl,
+  //       method: 'GET',
+  //       dataType: 'json',
+  //       success: function (data) {
+  //         displayResults(data);
+  //       },
+  //       error: function (error) {
+  //         console.error('Error fetching data:', error);
+  //       }
+  //     });
+  //   }
+  // })
 
-$.ajax({
-  url: requestUrl,
-  method: 'GET',
-}).then(function (response) {
-  console.log('Ajax Reponse \n-------------');
-  console.log(response);
-});
+  // //displays results from API in new container
+  // function displayResults(data) {
+  //   var resultsContainer = $('#results-container');
+  //   resultsContainer.empty();
+  //   $.each(data, function (index, strDrink) {
+  //     //creates element targeting class "result-item"**
+  //     var resultElement = $('<div class= "result-item">');
+  //     // customize  to display the relevant data from the API response
+  //     var titleElement = $('<h3>').text(item.title);
+  //     var descriptionElement = $('<p>').text(item.description);
 
+  //     //append title and description to results element
+  //     resultsElement.append(titleElement, descriptionElement);
+  //     resultsContainer.append(resultElement);
 
-//  $.getJSON("https://https://thecocktaildb.com/api/json/v1/1/random.php", function (data) {
-//    var resources = data.resources.map(resource => `<a href="${resource.strDrink}">${resouce.strInstructions}</a><br?`),
-//     text = `<h2>`${data.resources}
-//   })
+  //     //click event
+  //     $('#search-button').click(function () {
+  //       fetchDataFromApi();
+  //     })
+  //   });
+  // }
 
+  // fetchDataFromApi();
 
+  //  $.getJSON("https://https://thecocktaildb.com/api/json/v1/1/random.php", function (data) {
+  //    var resources = data.resources.map(resource => `<a href="${resource.strDrink}">${resouce.strInstructions}</a><br?`),
+  //     text = `<h2>`${data.resources}
+  //   })
 
-
-
-
-
-
-  // console.log(fetch("www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"));
-
-  // fetch(https://thecocktaildb.com/api/json/v1/1/search.php?s=margarita)
 
 
 
@@ -70,4 +129,4 @@ $.ajax({
   //       // Handle any errors that occur during the API call
   //       console.error('Error fetching data:', error);
   //     });
-  // }
+}
