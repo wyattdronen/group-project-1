e885e981b3556164b686dcb9f5ffa5815933e6df
Jon A <jon.jumangi@gmail.com>
2023-07-19 20:43:23 -0500

    age-verification

--- assets/js/display-search.js
+++ assets/js/display-search.js
@@ -6,7 +6,7 @@ var fetchButton = document.getElementById('search-button');
 
 
 
-
+//random cocktail
 var requestUrl = 'https://thecocktaildb.com/api/json/v1/1/random.php';
 
 fetch(requestUrl)
@@ -14,9 +14,13 @@ fetch(requestUrl)
   .then(data => {
     const apiData = data;
     console.log(apiData)
+    //drink name from API
     const drinkName = data.drinks[0].strDrink;
+    //drink recipe instructions
     const drinkInst = data.drinks[0].strInstructions;
+    //drink image
     const drinkImage = data.drinks[0].strDrinkThumb
+    //ingredients 1-7
     const ingredientOne = data.drinks[0].strIngredient1;
     const ingredientTwo = data.drinks[0].strIngredient2;
     const ingredientThree = data.drinks[0].strIngredient3;
@@ -24,6 +28,7 @@ fetch(requestUrl)
     const ingredientFive = data.drinks[0].strIngredient5;
     const ingredientSix = data.drinks[0].strIngredient6;
     const ingredientSeven = data.drinks[0].strIngredient7;
+    //measurements of ingredients
     const measureOne = data.drinks[0].strMeasure1;
     const measureTwo = data.drinks[0].strMeasure2;
     const measureThree = data.drinks[0].strMeasure3;
@@ -43,16 +48,9 @@ fetch(requestUrl)
     document.getElementById("drink-result").innerHTML = drinkName;
     document.getElementById('ingredients').innerHTML = (measureOne + ingredientOne + " " + measureTwo + ingredientTwo + " " + measureThree + ingredientThree + " " + measureFour + ingredientFour + " " + measureFive + ingredientFive + " " + measureSix + ingredientSix + " " + measureSeven + ingredientSeven);
     document.getElementById("method").innerHTML = drinkInst;
-    // document.getElementById("hero").innerHTMl = drinkImage;
+    document.getElementById("drink-image").innerHTML = drinkImage;
   })
 
-
-// //create elements
-// const drinkNameEl = document.createElement('h2');
-// const drinkInstEl = document.createElement('p');
-// //set text of elements
-// drinkNameEl.textContent = Data.drinks[0].strDrink;
-// drinkInstEl.textContent = Data.drinks[0].strInstructions;
 
 
 
